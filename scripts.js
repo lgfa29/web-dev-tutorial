2132b154f0c9c0d0b1122b9e60b10a1f690bed83
Luiz Aoqui <lgfa29@gmail.com>
2015-06-24 13:53:50 -0700

    add jquery

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
-window.onload = function() {
-  var quotes = document.getElementsByClassName('quote');
-
-  for (var i = 0; i < quotes.length; i++) {
-    var quote = quotes[i];
-    quote.onclick = function(ev) {
-      alert('click');
-    };
-  }
-};
+$(document).ready(function() {
+  var quotes = $('.quote');
+  quotes.click(function(ev) {
+    alert('click');
+  });
+});
