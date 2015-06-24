2bdc365944ddde0b90544330b4b539ff769070b1
Luiz Aoqui <lgfa29@gmail.com>
2015-06-24 13:54:28 -0700

    add fancy jquery effect on click

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   var quotes = $('.quote');
   quotes.click(function(ev) {
-    alert('click');
+    var $target = $(ev.target);
+
+    // look for the .quote node if we click on a sub-node
+    if (!$target.hasClass('quote')) {
+      $target = $target.parent('p.quote');
+    }
+
+    $target.slideUp(1000, function() {
+      alert('faded out');
+    });
   });
 });
